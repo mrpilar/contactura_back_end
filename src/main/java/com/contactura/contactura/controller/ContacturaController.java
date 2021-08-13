502702f7850b8ecc65a521b0cba0d8aae2dc8f3c
Mario Pilar <mrpilar@gmail.com>
2021-08-13 15:17:23 -0300

    projeto corrigido

--- src/main/java/com/contactura/contactura/controller/ContacturaController.java
+++ src/main/java/com/contactura/contactura/controller/ContacturaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import com.contactura.contactura.model.Contactura;
 import com.contactura.contactura.repository.ContacturaRepository;
 
 @RestController
-@RequestMapping({"/contactura"}) // http://localhost:8090/contactura
+@RequestMapping({ "/contactura" }) // http://localhost:8090/contactura
 public class ContacturaController {
 
 	@Autowired
@@ -39,19 +40,25 @@ public class ContacturaController {
 	public Contactura create(@RequestBody Contactura contactura) {
 		return repository.save(contactura);
 	}
-	
+
 	// Update - http://localhost:8090/contactura/{id}
-	@PutMapping(value = "{id}")
-	public ResponseEntity update(@PathVariable long id,
-			@RequestBody Contactura contactura) {
-		return repository.findById(id)
-				.map(record -> {
-					record.setName(contactura.getName());
-					record.setEmail(contactura.getEmail());
-					record.setPhone(contactura.getPhone());
-					Contactura update = repository.save(record);
-					return ResponseEntity.ok().body(udpate);
-				}).orElse(ResponseEntity.notFound()).build());
+	public ResponseEntity update(@PathVariable long id, @RequestBody Contactura contactura) {
+		return repository.findById(id).map(record -> {
+			record.setName(contactura.getName());
+			record.setEmail(contactura.getEmail());
+			record.setPhone(contactura.getPhone());
+			Contactura update = repository.save(record);
+			return ResponseEntity.ok().body(update);
+		}).orElse(ResponseEntity.notFound().build());
 	}
-	
+
+	// Delete - http://localhost:8090/contactura/{id}
+	@DeleteMapping(path = { "/id" })
+	public ResponseEntity<?> delete(@PathVariable long id) {
+		return repository.findById(id).map(record -> {
+			repository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
 }
